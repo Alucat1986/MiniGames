2e6dba7fcd44119fc91ca5bfe7bef5d5e974a278
Alunya <tobias@hazardy.de>
2025-01-04 20:29:17 +0100

    Added a Game Class.

---
+++ Pong/src/Game/Game.cpp
@@ -0,0 +1,71 @@
+/**
+ * @file Game.cpp
+ * @brief Contains the implementation of the Game class.
+ * @author Alucat1986
+ * @date 04.01.2025
+ */
+
+#include <SFML/System/Clock.hpp>
+
+#include "Game.hpp"
+
+constexpr std::uint16_t WINDOW_WIDTH = 800u;
+constexpr std::uint16_t WINDOW_HEIGHT = 600u;
+
+ // ****************************************************************************************************************** //
+ //                                                                                                                    //
+ //                                                      CLASSES                                                       //
+ //                                                                                                                    //
+ // ****************************************************************************************************************** //
+
+namespace Pong
+{
+	/**
+	 * @brief Constructor.
+	 * @author Alucat1986
+	 * @date 04.01.2025
+	 */
+	Game::Game() :
+		m_IsRunning(true),
+		m_Score(0u),
+		m_Lives(3u)
+	{
+		m_Window = std::make_unique<sf::RenderWindow>(sf::VideoMode({ WINDOW_WIDTH, WINDOW_HEIGHT }), "Pong");
+		m_Window->setVerticalSyncEnabled(true);
+
+		m_Player = std::make_unique<Pong::PlayerPaddle>();
+		m_Enemy = std::make_unique<Pong::EnemyPaddle>();
+	} // Game::Game(...)
+
+	/**
+	 * @brief Main gameloop.
+	 * @author Alucat1986
+	 * @date 04.01.2025
+	 */
+	void Game::run()
+	{
+		sf::Clock clock;
+		while ( m_IsRunning )
+		{
+			sf::Time deltaTime = clock.restart();
+			
+			render(deltaTime.asSeconds());
+		} // while ( m_IsRunning )
+	} // void Game::run(...)
+
+	/**
+	 * @brief Renders the game.
+	 * @param[in] deltaTime Time since the last frame in seconds.
+	 * @author Alucat1986
+	 * @date 04.01.2025
+	 */
+	void Game::render(float deltaTime)
+	{
+		m_Window->clear();
+
+		m_Player->draw();
+		m_Enemy->draw();
+
+		m_Window->display();
+	} // void Game::render(...)
+} // namespace Pong
---
+++ Pong/src/Game/Game.hpp
@@ -0,0 +1,34 @@
+/**
+ * @file Game.hpp
+ * @brief Contains the declaration of the Game class.
+ * @author Alucat1986
+ * @date 04.01.2025
+ */
+
+#ifndef GAME_HPP
+#define GAME_HPP
+
+#include <SFML/Graphics/RenderWindow.hpp>
+
+#include "Paddles/PlayerPaddle.hpp"
+#include "Paddles/EnemyPaddle.hpp"
+
+namespace Pong
+{
+	class Game
+	{
+	public:
+		Game();
+		void run();
+	private:
+		bool									m_IsRunning;
+		std::uint16_t							m_Score;
+		std::uint16_t							m_Lives;
+
+		std::unique_ptr<sf::RenderWindow>		m_Window;
+		std::unique_ptr<Pong::PlayerPaddle>		m_Player;
+		std::unique_ptr<Pong::EnemyPaddle>		m_Enemy;
+	}; // class Game
+} // namespace Pong
+
+#endif
