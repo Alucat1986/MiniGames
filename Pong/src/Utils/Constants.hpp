79663bd57991ea995dff256bf8a682f4e77a7bdf
Alunya <tobias@hazardy.de>
2025-01-04 21:30:54 +0100

    Moved the Constexpr from Game/Game.cpp to Utils/Constants.hpp.
    I did this so I can access global constexpr where I need them.

---
+++ Pong/src/Utils/Constants.hpp
@@ -0,0 +1,16 @@
+/**
+ * @file Constants.hpp
+ * @brief Contains some constants for the game.
+ * @author Alucat1986
+ * @date 04.01.2025
+ */
+
+#ifndef CONSTANTS_HPP
+#define CONSTANTS_HPP
+
+#include <cstdint>
+
+constexpr std::uint16_t WINDOW_WIDTH = 800u;
+constexpr std::uint16_t WINDOW_HEIGHT = 600u;
+
+#endif
