d59b3f9c62afb73545570885f5d37a728261a9d7
Alunya <tobias@hazardy.de>
2025-06-09 14:52:03 +0200

    Implemented Skeleton classes for Animation and Renderer.
    They will be used to draw the snake and food onto the screen, so the snake itself doesn't need to take care of it itself anymore.

---
+++ Snake/Include/Graphics/Animation.hpp
@@ -0,0 +1,31 @@
+/**
+ * @file Animation.hpp
+ * @brief Contains the Animation class declaration.
+ * @author Alunya
+ * @date 09.06.2025
+ */
+
+#ifndef ANIMATION_HPP
+#define ANIMATION_HPP
+
+namespace snake {
+
+/**
+ * @class Animation
+ * @brief Manages the animations for game objects.
+ * @author Alunya
+ * @date 09.06.2025
+ *
+ * The animation class is responsible for handling the animations of game objects.
+ */
+class Animation {
+public:
+    Animation();
+    ~Animation() = default;
+
+private:
+};
+
+} // namespace snake
+
+#endif // ANIMATION_HPP
