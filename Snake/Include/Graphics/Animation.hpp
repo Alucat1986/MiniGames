587a2bcca8609368b237c06788e5176338bda1d9
Alunya <tobias@hazardy.de>
2025-06-09 17:47:01 +0200

    Implementation of the basic functionality of the Animation class.

--- Snake/Include/Graphics/Animation.hpp
+++ Snake/Include/Graphics/Animation.hpp
@@ -8,7 +8,13 @@
 #ifndef ANIMATION_HPP
 #define ANIMATION_HPP
 
+#include <SFML/Graphics/Sprite.hpp>
+
+#include <cstdint>
+#include <string>
+
 namespace snake {
+using std::uint32_t;
 
 /**
  * @class Animation
@@ -20,10 +26,20 @@ namespace snake {
  */
 class Animation {
 public:
-    Animation();
+    Animation( const sf::Texture& texture, const std::string& name, uint32_t frameCount = 1, uint32_t interval = 1 );
     ~Animation() = default;
 
+    void               update();
+    const sf::Sprite&  sprite() const;
+    const std::string& name() const;
+
 private:
+    sf::Sprite  mSprite;
+    std::string mName;
+    uint32_t    mFrameCount;
+    uint32_t    mCurrentFrame;
+    uint32_t    mInterval;
+    uint32_t    mCurrentIntervalStep;
 };
 
 } // namespace snake
--- Snake/Source/Graphics/Animation.cpp
+++ Snake/Source/Graphics/Animation.cpp
@@ -6,6 +6,16 @@
  */
 
 #include "Include/Graphics/Animation.hpp"
+
+#include "Include/Utils/Constants.hpp"
+
+#include <SFML/Graphics/Rect.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/Texture.hpp>
+
+#include <cstdint>
+#include <string>
+
 namespace snake {
 using std::uint32_t;
 
@@ -13,6 +23,69 @@ using std::uint32_t;
 //                                                      PUBLIC                                                        //
 // ****************************************************************************************************************** //
 
+/**
+ * @brief Constructor.
+ * @author Alunya
+ * @date 09.06.2025
+ * @param[in] texture The texture to use for the animation sprite.
+ * @param[in] name The name of the animation.
+ * @param[in] frameCount The number of frames in the animation.
+ * @param[in] interval The frame interval between animation frames.
+ */
+Animation::Animation( const sf::Texture& texture, const std::string& name, uint32_t frameCount, uint32_t interval )
+        : mSprite( texture ),
+          mName( name ),
+          mFrameCount( frameCount ),
+          mCurrentFrame( 0 ),
+          mInterval( interval ),
+          mCurrentIntervalStep( 1 ) {
+    mSprite.setTextureRect(
+        { { 0, 0 }, { static_cast<int>( constants::CELL_SIZE ), static_cast<int>( constants::CELL_SIZE ) } } );
+} // Animation::Animation(...)
+
+/**
+ * @brief Updates the animation frame.
+ * @author Alunya
+ * @date 09.06.2025
+ */
+void Animation::update() {
+    if ( mFrameCount == 1 ) {
+        return;
+    } // if ( mFrameCount == 1 )
+
+    mCurrentIntervalStep++;
+    if ( mCurrentIntervalStep >= mInterval ) {
+        mCurrentIntervalStep = 1;
+        mCurrentFrame++;
+        if ( mCurrentFrame >= mFrameCount ) {
+            mCurrentFrame = 0;
+        } // if ( mCurrentFrame >= mFrameCount )
+
+        mSprite.setTextureRect(
+            { { static_cast<int>( mCurrentFrame * constants::CELL_SIZE ), 0 },
+              { static_cast<int>( constants::CELL_SIZE ), static_cast<int>( constants::CELL_SIZE ) } } );
+    } // if ( mCurrentIntervalStep >= mInterval )
+} // void Animation::update(...)
+
+/**
+ * @brief Returns the current sprite of the animation.
+ * @author Alunya
+ * @date 09.06.2025
+ * @return A const reference to the current sprite of the animation.
+ */
+const sf::Sprite& Animation::sprite() const {
+    return mSprite;
+} // sf::Sprite& Animation::sprite(...) const
+
+/**
+ * @brief Returns the name of the animation.
+ * @author Alunya
+ * @date 09.06.2025
+ * @return A const reference to the name of the animation.
+ */
+const std::string& Animation::name() const {
+    return mName;
+} // std::string& Animation::name(...) const
 
 // ****************************************************************************************************************** //
 //                                                       END                                                          //
