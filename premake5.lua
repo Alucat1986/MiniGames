a38ffb23e1ace0af013aa251cc412c762848416d
Alunya <tobias@hazardy.de>
2025-07-27 16:00:08 +0200

    Changed warning level to 4 and enabled the unsigned/signed conversion warnings in MSVC

--- premake5.lua
+++ premake5.lua
@@ -6,7 +6,9 @@ workspace "MiniGames"
 
 	-- Workspace-wide build options for MSVC
 	filter "system:windows"
-		buildoptions { "/EHsc", "/Zc:preprocessor", "/Zc:__cplusplus" }
+		buildoptions { "/EHsc", "/Zc:preprocessor", "/Zc:__cplusplus", "/w44365" }
+
+	warnings "Extra"
 
 	OutputDir = "%{cfg.system}-%{cfg.architecture}/%{cfg.buildcfg}"
 
